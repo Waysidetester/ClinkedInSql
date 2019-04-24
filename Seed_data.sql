24d1ec5a573a979d0df5938d4eeb96e6e3bcc4e7
WaysideTester <dylanmurray211@outlook.com>
2019-04-23 21:17:02 -0500

    seed data

---
+++ Seed_data.sql
@@ -0,0 +1,56 @@
+insert into [Services] (Name, Description, Price)
+Values ('Intimdation', 'Im good at lookin tough', 2.50)
+
+insert into [Services] (Name, Description, Price)
+Values ('Skittles Tatoos', 'The finest skittle tatoos east of the missisippi', 4.25)
+
+insert into [Services] (Name, Description, Price)
+Values ('Cell Decoration', 'I make your cell look nice', 5.00)
+
+insert into [Services] (Name, Description, Price)
+Values ('Escape Route Planning', 'Ill help you get out of here', 3.75)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('bob', '2020/01/01', 60, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('steve', '2021/01/01', 45, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('jessi', '2022/01/01', 34, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('dylan', '2023/01/01', 7, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('nathan', '2024/01/01', 76, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('martin', '2025/01/01', 23, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('mary', '2026/01/01', 11, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('rich', '2027/01/01', 88, 1)
+
+Insert into Users (Name, ReleaseDate, Age, IsPrisoner)
+Values ('adam', '2028/01/01', 600, 1)
+
+INSERT into Interests (Name)
+Values ('Toilet Wine Crafting')
+
+INSERT into Interests (Name)
+Values ('Shank Forging')
+
+INSERT into Interests (Name)
+Values ('Shank Wielding')
+
+INSERT into Interests (Name)
+Values ('Flower Smelling')
+
+INSERT into Interests (Name)
+Values ('Intense Scrubbing of walls')
+
+INSERT into Interests (Name)
+Values ('Hooting at new incoming prisoners')
