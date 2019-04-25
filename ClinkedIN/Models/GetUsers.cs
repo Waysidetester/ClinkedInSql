84e1c79ad2b59c795132645c9259c7fa5e61d267
RichFisher <rich.fishernc@gmail.com>
2019-04-24 20:16:04 -0500

    checking dylans work

---
+++ ClinkedIN/Models/GetUsers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Models
+{
+    public class GetUsers
+    {
+        const string ConnectionString = "Server = localhost; Database = ClinkedIn; Trusted_Connection = True;";
+        
+        public List<User> GetAllUsers()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var getUsersCmd = connection.CreateCommand();
+                getUsersCmd.CommandText = ;
+                throw new Exception("Ya blew it!");
+            }
+        }
+        
+    }
+}
