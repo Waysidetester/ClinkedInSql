1800912417de1e4af885426528d34800f40299b4
WaysideTester <dylanmurray211@outlook.com>
2019-04-24 19:28:20 -0500

    adding a service successfully

--- ClinkedIN/Controllers/ServiceController.cs
+++ ClinkedIN/Controllers/ServiceController.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClinkedIN.Data;
+using ClinkedIN.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClinkedIN.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class ServiceController : ControllerBase
     {
+        readonly ServiceRepository _serviceRepository;
+
+        public ServiceController()
+        {
+            _serviceRepository = new ServiceRepository();
+        }
+
+        public ActionResult<SuccessfulAddedService> AddService (CreateServiceRequest serviceToAdd)
+        {
+            var newService = _serviceRepository.AddService(serviceToAdd.Name, serviceToAdd.Description, serviceToAdd.Price);
+
+            return newService;
+        }
     }
 }
--- ClinkedIN/Models/SuccessfulAddedService.cs
+++ ClinkedIN/Models/SuccessfulAddedService.cs
@@ -1,6 +1,10 @@
-﻿namespace ClinkedIN.Controllers
+﻿namespace ClinkedIN.Models
 {
     public class SuccessfulAddedService
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
